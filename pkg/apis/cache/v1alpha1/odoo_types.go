a7ed5345651ffba48938852cecb4e86989a9e157
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-07-05 17:33:11 -0500

    [ADD] After running `operator-sdk add api --api-version=cache.example.com/v1alpha1 --kind=Odoo`

---
+++ pkg/apis/cache/v1alpha1/odoo_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OdooSpec defines the desired state of Odoo
+type OdooSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// OdooStatus defines the observed state of Odoo
+type OdooStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Odoo is the Schema for the odoos API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=odoos,scope=Namespaced
+type Odoo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OdooSpec   `json:"spec,omitempty"`
+	Status OdooStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OdooList contains a list of Odoo
+type OdooList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Odoo `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Odoo{}, &OdooList{})
+}
